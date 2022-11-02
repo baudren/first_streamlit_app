21d3554ef6596de34b98d6a0a8441337d3984901
Benjamin Audren <benjamin.audren@elca.ch>
2022-11-02 12:03:07 +0100

    kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -22,5 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
