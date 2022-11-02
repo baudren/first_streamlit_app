5a03630257736149d651209ecdb7d7937cdc9991
Benjamin Audren <benjamin.audren@elca.ch>
2022-11-02 14:20:50 +0100

    tutu

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
     return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
